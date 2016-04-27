52f14a1ebfb3a3765dee1a45bf6a821a6f30541c
mrDarky <mr.pesotskyi@mail.ru>
2016-04-27 23:18:09 +0300

    add skelet temlate

--- webui/app.py
+++ webui/app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/leaves')
+def leaves():
+    return render_template('leaves.html')
+    
+@app.route('/artifacts')
+def artifacts():
+    return render_template('artifacts.html')
 
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+    
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('projects.html')
     
 if __name__ == "__main__":
     app.run()
