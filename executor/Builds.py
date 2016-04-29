7f2bfddb4aa4e8ac7dada0a43a5dd60487aa3d16
Rostyslav <rostawesomegd@gmail.com>
2016-04-29 19:11:43 +0300

    Finalize builds API

--- executor/Builds.py
+++ executor/Builds.py
@@ -16,11 +16,11 @@ class BuildDTO:
         }
 
 class BuildDAO:
-    # TODO: implement BuildDAO with sqlite backend
     def __init__(self, mongoDb):
         self.__builds = dict()
         self.__currentId = 1
         self.__mongoCollection = mongoDb['builds']
+        self.__numRecentBuilds = 20
 
     def addBuild(self, build):
         if self.checkBuild(build):
@@ -33,7 +33,27 @@ class BuildDAO:
         return self.__mongoCollection.distinct("buildId")
 
     def getBuild(self, buildId):
-        return list(self.__mongoCollection.find({'buildId':buildId}))
+        ret = list(self.__mongoCollection.find({'buildId':buildId}))
+        for r in ret:
+            del r['_id']
+        return ret
+
+    def getBuildByProject(self, projectId):
+        ret = list(self.__mongoCollection.find({'projectId':projectId}))
+        for r in ret:
+            del r['_id']
+        return ret
+
+    def getRecentBuilds(self, projectId=-1):
+        cursor = None
+        if projectId == -1:
+            cursor = self.__mongoCollection.find().limit(self.__numRecentBuilds)
+        else:
+            cursor = self.__mongoCollection.find({'projectId':projectId}).limit(self.__numRecentBuilds)
+        ret = list(cursor.sort([('timestamp', mongo.DESCENDING)]))
+        for r in ret:
+            del r['_id']
+        return ret
 
     def checkBuild(self, build):
         # TODO
@@ -48,14 +68,20 @@ class BuildIndex(Resource):
 
     def get(self):
         args = self.__parser.parse_args()
-        print(args)
-        ids = [b for b in storage.getBuildIds() and
-               storage.getBuild(b).projectId == args['projectId']]
-        print(ids)
+        ids = storage.getBuildByProject(args['projectId'])
         return {"builds":ids}
 
 class BuildRecentIndex(Resource):
-    pass
+    def __init__(self):
+        self.__parser = reqparse.RequestParser()
+        self.__parser.add_argument('projectId',required=False, type=int)
+
+    def get(self):
+        args = self.__parser.parse_args()
+        if args['projectId'] is not None:
+            return storage.getRecentBuilds(args['projectId'])
+        else:
+            return storage.getRecentBuilds()
 
 class BuildResource(Resource):
     def get(self, buildId):
